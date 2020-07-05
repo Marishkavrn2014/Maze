5db83f14db1b9938a136136afd7f7a70d41298dd
Marina Shadrina <marishka_vrn@list.ru>
2020-07-05 21:39:38 +0300

    dyrectory deleted

--- src/main/java/ms/maze/GameTests.java
+++ src/main/java/ms/maze/GameTests.java
@@ -3,75 +3,57 @@ package ms.maze;
 import ms.maze.controller.MazeGameController;
 
 import java.io.*;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import static java.lang.System.exit;
 
 //testing the game to calculate the average number of steps
 public class GameTests {
 
     public static void main(String[] args) throws IOException {
-        if(args.length < 2) {
-            System.out.print("Usage: please write a given time as an argument and field.txt");
-            System.exit(0);
+        if(args.length < 1) {
+            System.out.print("Usage: please write a name for the text file as field.txt");
+            exit(0);
         } else {
-            if(!args[0].matches("[0-9]")) {
-                System.out.print("Please, write a number");
-                System.exit(0);
-            }
-            File file = new File(System.getProperty("user.dir") + System.getProperty("file.separator") + args[1]);
+
+            File file = new File(System.getProperty("user.dir") + System.getProperty("file.separator") + args[0]);
             if(!file.canRead()) {
-                System.out.print("File " + args[1] + " cannot be read");
+                System.out.print("File " + args[0] + " cannot be read");
+                exit(0);
             }
         }
-        String fileName = System.getProperty("user.dir") + System.getProperty("file.separator") + args[1];
-        int steps = 0;
-        int n = 0, m = 0;
-        int time = Integer.parseInt(args[0]);
-        int count = 10*10;
+        String fileName = System.getProperty("user.dir") + System.getProperty("file.separator") + args[0];
         String resultGame = null;
-
-        while (steps == 0 || count == 0) {
-            for(int j = 2; j <= 10; j++) {
-                if(steps == 0) {
-                for (int k = 2; k <= 10; k++) {
-                    //start the game
-                    MazeGameController mazeGame = new MazeGameController();
-
-                    //get steps count
-                    steps = mazeGame.startGame(j, k, time, fileName);
-                    resultGame = mazeGame.getResult();
-
-                    if ("Congratulations!!! You win!!!\n".equals(resultGame)) {
-                        //save n and m position
-                        n = j;
-                        m = k;
-                        break;
-                    }
-                }
-                } else break;
-            }
-            count--;
-        }
-        //calculate average
 
         //writing the results to the file "test1.txt
         try(BufferedWriter bw = new BufferedWriter(new FileWriter(new File
                 (System.getProperty("user.dir") + System.getProperty("file.separator") + "test1.txt"), true))) {
-            if(n != 0) {
-                String result = args[1] + "\n" +
-                        "The ball can reach the end of the maze at a given time" +
-                        "\n" +
-                        "Given time(the number of steps at the beginning): " +
-                        time +
-                        " lockable partitions n(steps): " +
-                        n +
-                        " opening partitions m(steps): " +
-                        m +
-                        " steps: " +
-                        steps;
-                bw.newLine();
-                bw.write(result);
-            } else {
-                String result = "The ball could not reach the end of the maze at a given time\n";
-                bw.write(result);
+            for(int j = 2; j <= 10; j++) {
+                for (int k = 2; k <= 10; k++) {
+                    for (int t = 0; t < 100; t++) {
+                        int count = 1000;
+                        while (count != 0) {
+                            //start the game
+                            MazeGameController mazeGame = new MazeGameController();
+                            mazeGame.startGame(j, k, t, fileName);
+                            //get steps count
+                            resultGame = mazeGame.getResult();
+
+                            if (!"Congratulations!!! You win!!!\n".equals(resultGame)) {
+                                //save n and m position
+                                break;
+                            }
+                            count--;
+                        }
+
+                        if (count == 0) {
+                            String s = "\nFor opening partition n = " + j + " times, lockable partition m = " + k + " times, MIN TIME = " + t;
+                            bw.write(s);
+                            break;
+                        }
+                    }
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
